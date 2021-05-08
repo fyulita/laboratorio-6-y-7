201ce7208dbb1c0bad19b0c5ed6a7e8138cb1647
fyulita <federico_yulita@hotmail.com>
2021-05-08 19:33:47 -0300

    Avance con el Arduino

--- Arduino/PID-Control/PID-Control.ino
+++ Arduino/PID-Control/PID-Control.ino
@@ -1,5 +1,5 @@
 // e(t) = r(t) - i(t)
-// u(t) = Kp * e(t) + Ki * int(e(t), t) + Kd * D(e(t), t)   
+// feedback(t) = Kp * e(t) + Ki * int(e(t), t) + Kd * D(e(t), t)   
 
 // Import PIDController library
 #include <PIDController.h>
@@ -8,18 +8,22 @@ PIDController pid;
 // Define variables for the program.
 
 // PID parameters
-const int Kp = 1;
-const int Ki = 1;
-const int Kd = 1;
+const double Kp = 5;
+const double Ki = 0;
+const double Kd = 1;
 
 // Pins
 const int ledPin = 9;  
 const int sensorPin = 0;
 
 // Signals
-const int r = 112;
+const int r = 72;
 int lightLevel;
-int u;
+int feedback;
+
+// Callibration parameters
+const double a = 0;
+const double b = 1;
 
 void setup() {
   pinMode(ledPin, OUTPUT);  // LED
@@ -28,20 +32,34 @@ void setup() {
   pid.begin();              // Initialize the PID instance.
   pid.setpoint(r);          // The "goal" the PID controller tries to "reach".
   pid.tune(Kp, Ki, Kd);     // Tune the PID, arguments: Kp, Ki, Kd.
-  pid.limit(0, 255);        // Limit the PID output between 0 and 255, this is important to get rid of integral windup!
+
+  analogWrite(ledPin, 0);   // Initial perturbation.
 }
 
 void loop() {
-
   // Measure light level as a number between 0 (dark) and 255 (bright).
   lightLevel = analogRead(sensorPin); // Photoresistor, integer value between 0 and 1023.
+  lightLevel = map(lightLevel, 0, 1023, 0, 255);
+
+  lightLevel = callibration(a, b, lightLevel);
+
+  Serial.println(F("Light level"));
+  Serial.println(lightLevel);
   
-  u = pid.compute(lightLevel);        // Let the PID compute the value, returns the optimal output.
+  feedback = pid.compute(lightLevel);        // Let the PID compute the value, returns the optimal output.
+  int newLightLevel = lightLevel + feedback;
 
   // Use signal to feed the LED.
-  analogWrite(ledPin, u);
+  analogWrite(ledPin, newLightLevel);
 
-  Serial.println(u);
+  Serial.println(F("New light level"));
+  Serial.println(newLightLevel);
+  Serial.println(F("\n"));
 
   delay(1000);
+}
+
+int callibration(a, b, x) {
+  int y = a + b * x;
+  return y;
 }
