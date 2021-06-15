5153082ca59e880641ca065b1d88398604c1a093
fyulita <federico_yulita@hotmail.com>
2021-06-14 23:22:54 -0300

    Agrego algunas mediciones del Arduino con el compost que dejamos en el labo y el cuaderno que hice

---
+++ Arduino/arduino-code/arduino-code.ino
@@ -0,0 +1,42 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Pin donde se conecta el bus 1-Wire
+const int pinDatosDQ = 2;
+const int pinRelay = 7;
+
+// Instancia a las clases OneWire y DallasTemperature
+OneWire oneWireObjeto(pinDatosDQ);
+DallasTemperature sensorDS18B20(&oneWireObjeto);
+ 
+void setup() {
+    // Iniciamos la comunicación serie
+    Serial.begin(9600);
+    // Iniciamos el bus 1-Wire
+    sensorDS18B20.begin(); 
+
+    //Para el relay:
+    pinMode(pinRelay, OUTPUT); //conexión a S del relay
+}
+ 
+void loop() {
+    // CODIGO PARA EL SENSOR
+    sensorDS18B20.requestTemperatures();
+    // Leemos y mostramos los datos de los sensores DS18B20
+    Serial.print(sensorDS18B20.getTempCByIndex(0));
+    Serial.print(",");
+    Serial.print(sensorDS18B20.getTempCByIndex(1));
+    Serial.print(",");
+
+
+    if(sensorDS18B20.getTempCByIndex(1) <= 50){
+      digitalWrite(pinRelay, HIGH); //turn relay ON
+      Serial.println("Prendido");
+    }
+    else{
+      digitalWrite(pinRelay, LOW); //turn relay OFF
+      Serial.println("Apagado");
+    }
+    
+    delay(1000); 
+}
